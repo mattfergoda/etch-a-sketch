32d9dcdec0ac16380a7a4c8479940b76e16a3c7d
Matt Fergoda <mrfergoda@gmail.com>
2020-04-19 10:59:46 -0700

    Initial functionality works. Added initial UI.

---
+++ scripts.js
@@ -0,0 +1,70 @@
+let colorize = false;
+
+function createGrid(grid_size) {
+    // First find grid div.
+    const grid = document.querySelector('#grid');
+    grid.style.gridTemplateColumns = `repeat(${grid_size}, auto)`;
+
+    for (let i = 0; i < grid_size; i++) {
+        for (let j = 0; j < grid_size; j++) {
+            const newSquare = document.createElement('div');
+            newSquare.classList.add('square')
+            newSquare.addEventListener("mouseenter", (e) => {
+                if (!newSquare.style.backgroundColor && colorize) {
+                    newSquare.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+                }
+                else if (!newSquare.style.backgroundColor && !colorize) {
+                    newSquare.style.backgroundColor = 'black';
+                }
+                else if (newSquare.style.backgroundColor && colorize) {
+                    // Make color darker if already passed over square.
+                    newSquare.style.filter += 'brightness(70%)';
+                }
+            });
+            grid.appendChild(newSquare);
+        }
+    }
+}
+
+function addButtonListeners() {
+    const clearButton = document.querySelector('#clear');
+    clearButton.addEventListener('click', clearGrid);
+
+    const colorizeButton = document.querySelector('#colorize');
+    colorizeButton.addEventListener('click', colorizeSquares);
+
+    const blackButton = document.querySelector('#black');
+    blackButton.addEventListener('click', blackSquares);
+}
+
+function blackSquares(e) {
+    colorize = false;
+    clearGrid(e);
+}
+
+function colorizeSquares(e) {
+    colorize = true;
+    clearGrid(e);
+}
+
+function clearGrid(e) {
+    const grid = document.querySelector('#grid');
+
+    // Remove old grid.
+    for (let i = 0; i < grid_size ** 2; i++) {
+        grid.removeChild(grid.firstChild); 
+    }
+
+    promptString = "Enter the number of squares per side for the new grid. It must be less than 200 and actually be a number."
+    grid_size = prompt(promptString);
+    while (Math.abs(grid_size) >= 200 || isNaN(grid_size)) {
+        grid_size = prompt(promptString);
+    }
+    grid_size = Math.floor(Math.abs(grid_size));
+
+    createGrid(grid_size);
+}
+
+let grid_size = 50;
+createGrid(grid_size);
+addButtonListeners();
